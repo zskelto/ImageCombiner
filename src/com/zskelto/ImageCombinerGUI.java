f1762093d48fa4999fdcda26acbd0ba21eeda463
Zachary Skelton <zskelto@g.clemson.edu>
2021-03-21 11:22:39 -0400

    Added Cursor Capturing

--- src/com/zskelto/ImageCombinerGUI.java
+++ src/com/zskelto/ImageCombinerGUI.java
@@ -1,36 +1,66 @@
 package com.zskelto;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class ImageCombinerGUI extends JFrame {
     private JButton base_layer_load, top_layer_load;
     private JButton base_layer_show, top_layer_show;
-    private JButton combine, back;
+    private JButton combine,save;
     private JLabel displayImage;
-    private JPanel buttons;
+    private JLabel currentCoords;
+    private JPanel coords, setupButtons, combineButtons, image;
 
     public ImageCombinerGUI(){
+        //Create Buttons
         base_layer_load = new JButton("Load Base Image");
         top_layer_load = new JButton("Load Top Image");
         base_layer_show = new JButton("Show Base Image");
         top_layer_show = new JButton("Show Top Image");
+        combine = new JButton("Combine");
+        save = new JButton("Save");
 
+        //Add Buttons to Panels
+        setupButtons = new JPanel();
+        combineButtons = new JPanel();
+
+        setupButtons.add(base_layer_load);
+        setupButtons.add(base_layer_show);
+        setupButtons.add(top_layer_load);
+        setupButtons.add(top_layer_show);
+
+        combineButtons.add(combine);
+        combineButtons.add(save);
+
+        //Create Panel for Showing Mouse Coordinates
+        coords = new JPanel();
+        currentCoords = new JLabel("x:0 , y:0");
+        currentCoords.setOpaque(true);
+        currentCoords.setFont(new Font(currentCoords.getFont().getName(),Font.PLAIN, 14));
+        currentCoords.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        currentCoords.setSize(new Dimension(120,120));
+        coords.add(currentCoords);
+
+        //Create Image Display Panel
         displayImage =  new JLabel();
+        displayImage.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
 
-        buttons = new JPanel();
-        buttons.add(base_layer_load);
-        buttons.add(base_layer_show);
-        buttons.add(top_layer_load);
-        buttons.add(top_layer_show);
+        image = new JPanel();
+        image.add(displayImage);
 
-        setTitle("ImageCombiner");
-        setSize(800,800);
 
-        this.getContentPane().add(buttons, BorderLayout.PAGE_START);
-        this.getContentPane().add(displayImage,BorderLayout.CENTER);
+        //Set Frame Settings
+        this.setTitle("ImageCombiner");
+        this.setSize(800,800);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        //Add Panels
+        this.getContentPane().add(setupButtons, BorderLayout.PAGE_START);
+        this.getContentPane().add(coords,BorderLayout.WEST);
+        this.getContentPane().add(image,BorderLayout.CENTER);
+        this.getContentPane().add(combineButtons,BorderLayout.PAGE_END);
     }
 
     public void setBaseLayerLoadListener(ImageCombinerListener a){
@@ -48,6 +78,8 @@ public class ImageCombinerGUI extends JFrame {
     public void setTopLayerShowListener(ImageCombinerListener a){
         top_layer_show.addActionListener(a);
     }
+
+    public void setImageMouseListener (ImageCombinerListener a) { displayImage.addMouseListener(a); }
 
     public JButton getBaseLayerLoad(){
         return this.base_layer_load;
@@ -68,6 +100,8 @@ public class ImageCombinerGUI extends JFrame {
     public void setImage(ImageIcon icon){
         displayImage.setIcon(icon);
     }
+
+    public void setText(String s) {currentCoords.setText(s);}
 
     public void startGUI(){
         this.setVisible(true);
--- src/com/zskelto/ImageCombinerListener.java
+++ src/com/zskelto/ImageCombinerListener.java
@@ -1,14 +1,17 @@
 package com.zskelto;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 
-public class ImageCombinerListener implements ActionListener {
+public class ImageCombinerListener implements ActionListener, MouseListener {
     private ImageCombiner test;
     private ImageCombinerGUI gui;
+    private boolean baseDisplaying;
+    private int x,y;
 
     public ImageCombinerListener() {
+        baseDisplaying = false;
         gui = new ImageCombinerGUI();
         test = new ImageCombiner();
 
@@ -16,9 +19,11 @@ public class ImageCombinerListener implements ActionListener {
         gui.setTopLayerLoadListener(this);
         gui.setBaseLayerShowListener(this);
         gui.setTopLayerShowListener(this);
+        gui.setImageMouseListener(this);
         gui.startGUI();
     }
 
+    @Override
     public void actionPerformed(ActionEvent e){
         if (e.getSource() == gui.getBaseLayerLoad()){
             test.setBaseLayer();
@@ -27,6 +32,7 @@ public class ImageCombinerListener implements ActionListener {
             if(test.getBaseLayer() != null) {
                 ImageIcon icon = new ImageIcon(test.getBaseLayer());
                 gui.setImage(icon);
+                baseDisplaying = true;
             }
         }
         else if(e.getSource() == gui.getTopLayerLoad()){
@@ -36,7 +42,34 @@ public class ImageCombinerListener implements ActionListener {
             if(test.getTopLayer() != null) {
                 ImageIcon icon = new ImageIcon(test.getTopLayer());
                 gui.setImage(icon);
+                baseDisplaying = false;
             }
         }
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(baseDisplaying) {
+            x = e.getX();
+            y = e.getY();
+            gui.setText("x: " + x + " y:" + y);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
